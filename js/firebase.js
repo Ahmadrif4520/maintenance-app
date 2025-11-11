7c93066d61c12d4c370d63ba24184021dd34aa60
Ahmad Rifai <93535625+Ahmadrif4520@users.noreply.github.com>
2025-11-11 15:51:17 +0700

    Remove unnecessary whitespace in firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,25 +1,16 @@
+
 // js/firebase.js
 // Firebase configuration Anda dari konsol Firebase
 const firebaseConfig = {
-
   apiKey: "AIzaSyDjB3h7EsI-A7B56bkgvcKDVgvMJYSCjPU",
-
   authDomain: "chinaglaze-app.firebaseapp.com",
-
   databaseURL: "https://chinaglaze-app-default-rtdb.firebaseio.com",
-
   projectId: "chinaglaze-app",
-
   storageBucket: "chinaglaze-app.firebasestorage.app",
-
   messagingSenderId: "546767539175",
-
   appId: "1:546767539175:web:c67b1cd90638e8cc4fcb42",
-
   measurementId: "G-6PZN326DJ0"
-
 };
-
 
 // Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
@@ -32,3 +23,4 @@ export const db = firebase.firestore();
 export const firebase_firestore_FieldValue = firebase.firestore.FieldValue;
 
 console.log("Firebase initialized.");
+
